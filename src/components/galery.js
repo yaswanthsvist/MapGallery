53e0983e773e3c95142e46bc313919b748a81c9d
yaswanthsvist@gmail.com <yaswanth.m@scarletwireless.com>
2017-07-28 06:39:19 +0530

    markerview hasa added

--- src/components/galery.js
+++ src/components/galery.js
@@ -20,37 +20,84 @@ class Galery extends React.Component {
   };
   constructor(props) {
     super(props)
-
+    this.imageMarkers=[];
     this.props.dispatch({type:"REMOVE_ALL_IMAGES"})
     this.getImages();
   }
-  render() {
-    console.log(this.props);
-    let {images}=this.props;
-    console.log(images)
-    return (
-    <ScrollView>
-      <View style={styles.container}>
-        {
-          images.map((image,i)=>{
+  async componentWillReceiveProps(nextProps) {
+    let {images}=nextProps;
+    console.log(images);
+    let imageMarkers=await Promise.all(images.filter( image => ( image.location != undefined ) )
+        .map((image,i)=>{
             console.log(image);
-            return (<View style={{width:50,borderWidth:1,height:50,backgroundColor:'rgba(0,0,0,0.1)'}}  key={i+'img'}>
-                    <Image style={{width:50,height:50}}  source={{uri:image.uri}}>
-                    </Image>
-                  </View>)
-          })
-        }
-      </View>
+            return new Promise((resolve,reject)=>(<Image onLoad={resolve} onerror={reject} source={{uri:image.uri}} style={{width:50,height:50}} ></Image>)
+            ).then((res)=>{console.log(res);}).catch((err)=>{console.log(err);});
+          }
+        )
+      ).then((res)=>{
+        console.log(res);
+      }).catch(()=>{
+        console.log("err");
+      });
+
+      console.log(imageMarkers);
+
+    //this.imageMarkers=await imageMarkers;
+  //  console.log(this.imageMarkers);
+  }
+  render() {
+    //console.log(this.props);
+    let {images}=this.props;
+    //console.log(images)
+
+    var initialLatLang={
+      latitude:37.78825,
+      longitude:-122.4324,
+    }
+    if(images.length >=1 && (images[0].location!=undefined) &&images[0].location.latitude!=undefined ){
+      initialLatLang.latitude=images[0].location.latitude;
+      initialLatLang.longitude=images[0].location.longitude;
+    }
+    console.log(this.imageMarkers);
+    return (
+    <View>
+      {
       <MapView
+        style={{position :'absolute',top:0,left:0,width:400,height:400}}
         initialRegion={{
-          latitude: 37.78825,
-          longitude: -122.4324,
+          latitude: initialLatLang.latitude,
+          longitude: initialLatLang.longitude,
           latitudeDelta: 0.0922,
           longitudeDelta: 0.0421,
         }}
-      />
-      </ScrollView>
+      >
+      {
+        images.filter(image=>(image.location!=undefined)).map((image,i)=>{
+          console.log(image);
+          return (
+            <MapView.Marker  key={i+'img'}
+            coordinate={image.location}
+
+            >
+              <View latlng={image.location} style={{width:50,borderWidth:1,height:50,backgroundColor:'rgba(0,0,0,0.1)'}}>
+                  <Image  source={{uri:image.uri}} style={{width:50,height:50}} ></Image>
+              </View>
+            </MapView.Marker>
+          )
+        })
+      }
+      </MapView>
+      }
+      <View>
+      {
+        //(this.state.imageMarkers!=null&&this.state.imageMarkers[0])||null
+      }
+      </View>
+      </View>
     );
+  }
+  async loadImages(){
+
   }
 
   async getImages(){
